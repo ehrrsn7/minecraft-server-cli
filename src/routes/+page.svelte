57bbec1a14464c2519573e07b8fb8acc333cb5c8
E Harrison <har16020@byui.edu>
2023-08-16 16:30:05 -0700

    minor changes to body and config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <h1>minecraft-server-cli</h1>
 
-<todo>
+— <todo>
    implement the cli using provided frameworks.
    <a href="https://www.telerik.com/blogs/how-to-build-cli-node-react">here is a good one</a>,
    although it seems like it's on the older side.
-</todo>
-<todo>
+</todo><br />
+— <todo>
    dockerize this application.
-</todo>
-<todo>
+</todo><br />
+— <todo>
    this app is svelte and react-enabled.
    should be great, but make sure that as fancy and pretty it is, it should be functional and *secure*.
-</todo>
+</todo><br />
